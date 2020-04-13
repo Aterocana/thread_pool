c4bb3c61e9268e2799e81a8d971dab94e1986418
Aterocana <dominicimaurizio@gmail.com>
2020-04-13 12:18:55 +0200

    refactoring worker in a sub crate.
    Signed-off-by: Aterocana <dominicimaurizio@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 pub use errors::PoolCreationError;
+use std::error::Error;
 use std::sync::{mpsc, Arc, Mutex};
-use std::{error::Error, thread};
 
 mod errors;
+mod worker;
 
 pub struct ThreadPool {
-    _workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    _workers: Vec<worker::Worker>,
+    sender: mpsc::Sender<worker::Job>,
 }
 
 impl ThreadPool {
@@ -18,7 +19,7 @@ impl ThreadPool {
         let rx = Arc::new(Mutex::new(rx));
         let mut _workers = Vec::with_capacity(size);
         for i in 0..size {
-            _workers.push(Worker::new(i, Arc::clone(&rx)));
+            _workers.push(worker::Worker::new(i, Arc::clone(&rx)));
         }
         Ok(ThreadPool { _workers, sender })
     }
@@ -31,34 +32,5 @@ impl ThreadPool {
 
         self.sender.send(job)?;
         Ok(())
-    }
-}
-
-// Box<dyn FnOnce() + Send + 'static> is a trait object;
-// FnOnce trait is a closure which will be executed once;
-// Send means the value can be sent across multiple trait;
-// 'static bound is the object lifetime.
-type Job = Box<dyn FnOnce() + Send + 'static>;
-
-struct Worker {
-    _id: usize,
-    _f: thread::JoinHandle<()>,
-}
-
-impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        Worker {
-            _id: id,
-            _f: thread::spawn(move || loop {
-                let job = receiver
-                    .lock()
-                    .expect("error occurred while trying to acquire lock")
-                    .recv()
-                    .expect("error occurred while trying to receive from channel");
-
-                println!("worker {} got a job: executing.", id);
-                job();
-            }),
-        }
     }
 }
--- src/worker.rs
+++ src/worker.rs
@@ -1,21 +1,22 @@
-use std::{error::Error, thread};
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread;
 
 // Box<dyn FnOnce() + Send + 'static> is a trait object;
 // FnOnce trait is a closure which will be executed once;
 // Send means the value can be sent across multiple trait;
 // 'static bound is the object lifetime.
-type Job = Box<dyn FnOnce() + Send + 'static>;
+pub type Job = Box<dyn FnOnce() + Send + 'static>;
 
 pub struct Worker {
-    id: usize,
-    f: thread::JoinHandle<()>,
+    _id: usize,
+    _f: thread::JoinHandle<()>,
 }
 
 impl Worker {
     pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         Worker {
-            id: id,
-            f: thread::spawn(move || loop {
+            _id: id,
+            _f: thread::spawn(move || loop {
                 let job = receiver
                     .lock()
                     .expect("error occurred while trying to acquire lock")
